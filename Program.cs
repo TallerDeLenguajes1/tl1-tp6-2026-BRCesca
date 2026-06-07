94fad0454532c155efe2252d99addc7f967ea39e
BrunoCesca <bruno.r.cesca@gmail.com>
2026-06-07 18:27:21 -0300

    calculadora v1 completa y gitignore actualizado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Xml.Schema;
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Ingrese el valor que desea invertir: ");
 int numeroParaInvertir;
 int numeroInvertido=0;
@@ -25,5 +27,78 @@ else
 }
 //------------------------------
 //Ejercicio 2
+int opcion=0;
+do
+{
 
+    Console.WriteLine("==================================================");
+    Console.WriteLine("       CALCULADORA V1     ");
+    Console.WriteLine("==================================================");
+    Console.WriteLine(" Seleccione la operacion que desea realizar:");
+    Console.WriteLine();
+    Console.WriteLine("   [1] -> SUMAR");
+    Console.WriteLine("   [2] -> RESTAR");
+    Console.WriteLine("   [3] -> MULTIPLICAR");
+    Console.WriteLine("   [4] -> DIVIDIR ");
+    Console.WriteLine("   [5] -> SALIR DEL PROGRAMA");
+    Console.WriteLine("==================================================");
+    Console.Write(" >> Ingrese su opcion (1-5): ");
+    while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 1 || opcion > 5)
+    {
+        Console.Write("Opcion no valida. Ingrese un numero del 1 al 5: ");
+    }
+    if(opcion !=5)
+    {
+        double num1, num2, resultado=0;
+        string operacion="";
+        Console.WriteLine("Ingrese el primer valor");
+        while (!double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.Write("Ingrese un numero valido: ");
+        }
+        Console.WriteLine("Ingrese el segundo valor: ");
+        while (!double.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.Write("Ingrese un numero valido: ");
+        }
+        switch (opcion)
+        {
+            case 1:
+                resultado = num1 + num2;
+                operacion = "suma";
+            break;
+        
+            case 2:
+                resultado = num1 - num2;
+                operacion = "resta";
+            break;
+        
+            case 3:
+                resultado = num1 * num2;
+                operacion = "multiplicacion";
+
+            break;
+        
+            case 4:
+                if (num2 != 0)
+                {
+                    resultado = num1/num2;
+                    operacion = "division";
+                }
+            break;
+        }
+        if(opcion == 4 && num2 == 0)
+        {
+            Console.WriteLine("Error. El denominador no puede ser 0");
+        }
+        else
+        {
+            Console.WriteLine($"El resultado de la {operacion} entre los numeros {num1} y {num2} es {resultado}");
+        }
+        Console.WriteLine("presione enter para continuar");
+        Console.ReadLine();        
+    }
+
+
+}while(opcion != 5);
 
